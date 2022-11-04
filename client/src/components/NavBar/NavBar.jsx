01f88931c34a23d851f3eb4bafb3de259d8b194f
Lautaro <lautiisantillan8@gmail.com>
2022-11-04 18:33:46 -0300

    frontend listo

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -6,10 +6,12 @@ import { Link } from "react-router-dom";
 export default function NavBar({ paginado }) {
   return (
     <div id={styles.header}>
-      <div id={styles.divContainer}>
-        <img id={styles.img} src={"https://cdn-icons-png.flaticon.com/128/6469/6469203.png"} alt="Recipe" width="50px" height="50px" />
-        <h2 id={styles.h1}>Recipes</h2>
-      </div>
+      <Link to={"/"}>
+        <div id={styles.divContainer}>
+          <img id={styles.img} src={"https://cdn-icons-png.flaticon.com/128/6469/6469203.png"} alt="Recipe" width="50px" height="50px" />
+          <h2 id={styles.h1}>Recipes</h2>
+        </div>
+      </Link>
       <div id={styles.divButtonCreate}>
         <Link to={"/recipes"}>
           <button id={styles.buttonCreate}>Create Recipe</button>
